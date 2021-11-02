dd1982b1b586f8ef34ec16beb0537f831936c3b1
KatarzynaKaczmarczyk1234 <92315919+KatarzynaKaczmarczyk1234@users.noreply.github.com>
2021-11-02 15:29:14 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("-----***Hello World $ welcom Jnkins ***-----");
+		System.out.println("-----*** Test zmiany ***-----");
 	}
 }
